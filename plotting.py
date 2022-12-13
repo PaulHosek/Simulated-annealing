f7b108a9d1748c8f86f013e237b355ce83445462
Paul Hosek <96589432+PaulHosek@users.noreply.github.com>
2022-12-13 13:59:41 +0100

    - convergence plotting funciton

--- plotting.py
+++ plotting.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
 import pandas as pd
-
+import seaborn as sns
 # Global vars
 p_idx = 0
 temp_index = 0
@@ -76,7 +76,7 @@ def animate_convergence(ch, low_temp, high_temp, n_temps, schedule, chain_length
     plt.show()
     # return anim
 
-def plot_convergence(filename):
+def plot_convergence_v1(filename):
     fig, ax = plt.subplots()
     data = pd.read_csv(filename)
     energy = data["Potential_energy"]
@@ -88,3 +88,37 @@ def plot_convergence(filename):
     ax2.plot(temp, color='red')
     ax2.set_ylabel('Temperature', color='red')
     plt.show()
+
+def plot_convergence(fname,pic_name,first_n_iters=None):
+    res_df = pd.read_csv(f"logged_data/{fname}.csv", skiprows=[1]).rename(columns={"Unnamed: 0": "Iterations"})
+    if first_n_iters:
+        res_df = res_df[:first_n_iters]
+
+
+    # calculate mean and 95% ci for temperature level
+    stats = res_df.groupby(['Temperatures']).agg(['mean', 'sem'])
+    x_iters = stats["Iterations"]['mean']
+    stats = stats["Potential_energy"]
+    stats['ci95_hi'] = stats['mean'] + 1.96 * stats['sem']
+    stats['ci95_lo'] = stats['mean'] - 1.96 * stats['sem']
+    stats = stats.iloc[::-1]
+
+    # draw
+    plt.figure(figsize=(12, 8))
+    sns.set_theme(style="whitegrid")
+
+    ax1 = sns.lineplot(x=x_iters, y=stats['mean'], sort=False, color='blue', label='Mean Energy in Chain')
+    sns.lineplot(ax=ax1, x=x_iters, y=stats['ci95_hi'], sort=False, color='grey', linestyle='--', label='95% CI')
+    sns.lineplot(ax=ax1, x=x_iters, y=stats['ci95_lo'], sort=False, color='grey', linestyle='--')
+    ax1.fill_between(x_iters[::-1], stats['mean'], stats['ci95_hi'], color='lightblue', alpha=0.5)
+    ax1.fill_between(x_iters[::-1], stats['mean'], stats['ci95_lo'], color='lightblue', alpha=0.5)
+    ax1.scatter(x_iters[::-1], stats['mean'], color='black', s=20)
+
+    sns.set_theme(style="white")
+    ax2 = ax1.twinx()
+    sns.lineplot(ax=ax2, x=res_df["Iterations"], y=res_df["Temperatures"], color='red')
+
+    ax1.set_ylabel("Potential Energie, E")
+    ax1.set_xlabel("Iterations")
+    plt.xlim((1, 3000))
+    plt.savefig(pic_name,dpi=300,bbox_inches='tight')
